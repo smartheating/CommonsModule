85260ff7ecdc6cc50bc852d0ae0954c24ebc52be
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-11 20:51:24 +0200

    Updated AWS configuration

--- src/main/java/de/smartheating/SmartHeatingCommons/configuration/AwsConfiguration.java
+++ src/main/java/de/smartheating/SmartHeatingCommons/configuration/AwsConfiguration.java
@@ -1,5 +1,6 @@
 package de.smartheating.SmartHeatingCommons.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,9 @@ import com.netflix.appinfo.AmazonInfo;
 
 @Configuration
 public class AwsConfiguration {
+	
+	@Value("${server.port}")
+	private Integer applicationPort;
 
 	@Bean
 	@Profile("aws")
@@ -17,6 +21,9 @@ public class AwsConfiguration {
 	  EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
 	  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
 	  config.setDataCenterInfo(info);
+	  config.setHostname(info.get(AmazonInfo.MetaDataKey.localHostname));
+	  config.setIpAddress(info.get(AmazonInfo.MetaDataKey.localIpv4));
+	  config.setNonSecurePort(applicationPort);
 	  return config;
 	}
 }
