56376e594db655dae64d016d139745bad1f0ddae
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-12 11:48:36 +0200

    Clean-up + new logs

--- src/main/java/de/smartheating/SmartHeatingCommons/restcommunication/RepositoryClient.java
+++ src/main/java/de/smartheating/SmartHeatingCommons/restcommunication/RepositoryClient.java
@@ -3,7 +3,6 @@ package de.smartheating.SmartHeatingCommons.restcommunication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -17,17 +16,16 @@ public class RepositoryClient {
 	
 	Logger logger = LoggerFactory.getLogger(RepositoryClient.class);
 	
-	@Value("${spring.profiles.active}")
-	private String activeProfile;
-	
 	@Autowired
 	RestTemplate rest;
 	
 	public Device addDevice(String url, Device device) throws RestClientException {	
+		logger.info("Calling Repository-Service to post a device");
 		return rest.postForObject(url + "/devices", new HttpEntity<>(device), Device.class);
 	}
 	
 	public Event addEvent(String url, Event event) throws RestClientException {	
+		logger.info("Calling Repository-Service to post a event");
 		return rest.postForObject(url + "/events", new HttpEntity<>(event), Event.class);
 	}
 
--- src/main/java/de/smartheating/SmartHeatingCommons/services/DiscoveryService.java
+++ src/main/java/de/smartheating/SmartHeatingCommons/services/DiscoveryService.java
@@ -2,6 +2,8 @@ package de.smartheating.SmartHeatingCommons.services;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -11,6 +13,8 @@ import javassist.NotFoundException;
 
 @Service
 public class DiscoveryService {
+	
+	Logger logger = LoggerFactory.getLogger(DiscoveryService.class);
 
 	@Autowired
 	DiscoveryClient discoveryClient;
@@ -24,6 +28,7 @@ public class DiscoveryService {
 		}
 
 		// Since we will only have one instance per service
+		logger.info("Returning URL '" + serviceInstances.get(0).getUri().toString() + "' for service with id '" + serviceId + "'");
 		return serviceInstances.get(0).getUri().toString();
 	}
 }
